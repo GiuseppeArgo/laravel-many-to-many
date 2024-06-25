bdb7aa4d4a89aad15749d947da68b29c0578c682
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-25 17:01:30 +0200

    feat: created relation many-to-many between tables and showed technologies in show.blade

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,5 +6,16 @@
         <p class="mt-5">Descrizione: {{ $project->description }}</p>
         <p class="mt-5">Slug: {{ $project->slug }}</p>
         <p>Tipo: {{ $project->type?->name }}</p>
+        <p>Tecnologie:
+            @if($project->technologies->isNotEmpty())
+                <ul>
+                    @foreach($project->technologies as $technology)
+                        <li>{{ $technology->name }}</li>
+                    @endforeach
+                </ul>
+            @else
+                <p>Nessuna tecnologia</p>
+            @endif
+        </p>
     </div>
 @endsection
